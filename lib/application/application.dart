a20b1939f3890b2a416b2f1c53a432fa39f651ba
Samir-Sattarov <samirsattarov2004@gmail.com>
2024-02-16 17:37:51 +0500

    Added comment in application

--- lib/application/application.dart
+++ lib/application/application.dart
@@ -1,16 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:test_project/screens/home_screen.dart';
-
-
 class Application extends StatelessWidget {
   const Application({super.key});
-
-
-
-
-  // TESTSTTSTE
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
